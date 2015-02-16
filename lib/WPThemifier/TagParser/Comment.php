17ec64e84cf349212a7f6ed915eb8f81879f3177
xemlock <xemlock@gmail.com>
2015-02-16 11:02:39 +0100

    added support for nested comments

--- lib/WPThemifier/TagParser/Comment.php
+++ lib/WPThemifier/TagParser/Comment.php
@@ -4,7 +4,22 @@ class WPThemifier_TagParser_Comment extends WPThemifier_TagParser_Abstract
 {
     public function parse(array $token, WPThemifier_Compiler $themifier)
     {
-        $themifier->read(array($this, 'tagStop'));
+        $stream = $themifier->getStream();
+
+        while ($token = $stream->next()) {
+            // handle nested comments
+            if ($token['type'] === WPThemifier_Token::TYPE_TAG_START &&
+                $token['tag'] === $this->getTag()
+            ) {
+                $this->parse($token, $themifier);
+            }
+
+            if ($token['type'] === WPThemifier_Token::TYPE_TAG_END &&
+                $token['tag'] === $this->getTag()
+            ) {
+                break;
+            }
+        }
     }
 
     public function getTag()
